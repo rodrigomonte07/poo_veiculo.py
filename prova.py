7a252eb13f0e99b06f86374c66fc722e89d6d735
RODRIGO MONTE COSTA <rodrigomontecosta@gmail.com>
2025-11-17 22:52:05 -0300

    Add files via upload

---
+++ prova.py
@@ -0,0 +1,23 @@
+class Veiculo():
+
+    def movimentar(self):
+        print("Veículo está em movimento.") 
+    
+class Carro(Veiculo):
+
+    def movimentar(self):
+        print("Carro está dirigindo.") 
+    
+class Moto(Veiculo):
+
+    def movimentar(self):
+        print("Moto está acelerando")
+    
+veiculo_1 = Veiculo()
+carro_1 = Carro()
+moto_1 = Moto()
+
+veiculo_1.movimentar()
+carro_1.movimentar()
+moto_1.movimentar()
+        
